e7e22cd9737ebb36e3a41dca339a0a87ffa5fc41
Evan Yan <evanyans@gmail.com>
2024-05-29 10:29:30 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `personal-website-v3`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-datocms',
+    options: {
+      "apiToken": "8c7a783037c7a4bf897b68197e3583"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss"]
+};
