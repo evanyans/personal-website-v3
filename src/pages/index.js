6ff06f1c8d99f49a4d4eb0340734ac923496b3a8
Evan Yan <evanyans@gmail.com>
2024-05-29 19:55:57 -0400

    Added image gallery

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,107 @@
-import * as React from "react"
-import NavBar from './navbar.js'
+import * as React from "react";
+import NavBar from "./navbar.js";
 
 const IndexPage = () => {
   return (
     <main class="container mx-auto px-4">
       {/* <Link to="/writing/">Writing</Link> */}
-      <NavBar/>
-      <div class="flex flex-col gap-2">
-        <h1 class="font-semibold">Evan Yan</h1>
-        <p>I'm a designer, gamer and engineer. I'm currently working as an AI software developer at 
-          the University of Waterloo, where I'm building the next job portal for WaterlooWorks.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-      </div> 
-      
+      <NavBar />
+      <div class="flex flex-col gap-4 mt-12">
+        <h1 class="font-semibold text-lg">Evan Yan <span class="text-2xl">&nbsp;🐳</span></h1>
+        <p>
+          I'm a designer, gamer and engineer. I'm currently working as an AI
+          software developer at the University of Waterloo, where I'm building
+          the next job portal for WaterlooWorks.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+
+     {/* move htis somewhere soon */}
+      <div class="grid grid-cols-2 md:grid-cols-3 gap-3 mt-12 ">
+        <div class="grid gap-4"> 
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="grid gap-4">
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="grid gap-4">
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              class="h-auto max-w-full rounded-lg"
+              src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+
 
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => <title>Evan Yan</title>
+export const Head = () => <title>Evan Yan</title>;
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby"
 
 const NavBar = () => {
   return (
-    <div class="flex flex-row gap-3 font-light">
+    <div class="flex flex-row gap-6 font-light mt-16">
         <Link to="/">About</Link>
         <Link to="/writing/">Writing</Link>
         <Link to="/writing/">Resume</Link>
