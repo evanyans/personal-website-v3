17b1834aa14742d462f22e64f344933cc3f4005a
Evan Yan <evanyans@gmail.com>
2024-06-14 14:32:36 -0400

    readonly api key

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
   plugins: [{
     resolve: 'gatsby-source-datocms',
     options: {
-      "apiToken":process.env.DATO_API_KEY
+      "apiToken": "8c7a783037c7a4bf897b68197e3583"
     }
   }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss",
   {
