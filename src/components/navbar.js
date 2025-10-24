ffb79b3ef9ce26beb19f4ee3aeefcc67f24b7635
Evan Yan <evanyans@gmail.com>
2025-10-24 11:46:40 -0400

    Update navbar.js
    Removed the Resume link from the navbar.

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,9 +7,6 @@ const NavBar = () => {
       <div class="flex flex-row gap-6 font-light">
           <Link to="/">About</Link>
           <Link to="/writing/">Writing</Link>
-          <a href="/Evan_Yan_Resume.pdf" target="_blank" rel="noopener noreferrer">
-            Resume
-          </a>
       </div>
     </main>
   )
